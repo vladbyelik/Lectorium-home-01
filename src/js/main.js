c9cd0d7a4b3eb9d5eacca86a8f3b8bb42aea64c6
vladbyelik <vladbyelik@gmail.com>
2019-10-12 23:46:43 +0300

    Finished projects and add js

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,28 @@
-$('.menu-btn').on('click', function (e) {
-    e.preventDefault();
-    $(this).toggleClass('menu-btn-active');
-    $('.menu-nav').toggleClass('menu-nav-active');
+$('.menu-btn, .menu-nav').on('click', function (e) {
+        e.preventDefault();
+        $(this).toggleClass('menu-btn-active');
+        $('.menu-nav').toggleClass('menu-nav-active');
+});
+
+
+$(function(){
+    $('.height').height($('.height').width()/3);
+
+    $(window).resize(function(){
+        $('.height').height($('.height').width()/3);
+    });
+});
+
+
+let arrow = document.getElementsByClassName('hover-arrow');
+let li = document.getElementsByClassName('list');
+let link = document.getElementsByClassName('link');
+
+$(function () {
+    for (let i =0; i<arrow.length; i++) {
+        $(li[i]).on('mouseover mouseout', function(){
+            $(arrow[i]).toggleClass('hover');
+            $(link[i]).toggleClass('hover-color');
+        });
+    }
 });
